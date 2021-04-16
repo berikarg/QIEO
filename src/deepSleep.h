b8bf791c676c8e45c2f484c71918884712100eb0
Berik Argimbayev <berikarg@gmail.com>
2021-04-16 17:39:10 +0600

    fixed mistake in deepsleep bitmask

--- src/deepSleep.h
+++ src/deepSleep.h
@@ -2,8 +2,7 @@
 #define DEEPSLEEP_H
 
 #define TIME_TO_SLEEP 300 //in seconds 300
-#define WAKEUP_PIN_BITMASK 0x0B00000000 // pins 32,33,35 are enabled
-//#define WAKEUP_PIN_BITMASK 0x1B00000000 // pins 32,33,35,36 are enabled
+#define WAKEUP_PIN_BITMASK 0x0D00000000 // pins 32,34,35 are enabled
 extern hw_timer_t * deepSleepTimer;
 
 void IRAM_ATTR onDeepSleepTimer();
